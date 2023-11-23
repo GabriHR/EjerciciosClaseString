20eccbaccc238362de002c516a34e4a79ac21a37
GabriHR <ghernrod@myuax.com>
2023-11-23 12:53:22 +0100

    Explicación del código del Ejercicio5.

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main() {
-    string cadena = "If you want to  be successful, you gotta be a constant worker.";
-    string subcadena = cadena.substr(17, 12);
-    string cadena_a_insertar = " and have a great life";
+    string cadena = "If you want to  be successful, you gotta be a constant worker.";//cadena original
+    string subcadena = cadena.substr(17, 12);//subcadena a buscar
+    string cadena_a_insertar = " and have a great life";//cadena a insertar
 
-    cadena.insert(cadena.find(subcadena) + subcadena.length(), cadena_a_insertar);
+    cadena.insert(cadena.find(subcadena) + subcadena.length(), cadena_a_insertar);//inserta la cadena
 
-    cout << "La cadena resultante es: " << cadena << endl;
+    cout << "La cadena resultante es: " << cadena << endl;//imprime la cadena resultante
 
     return 0;
 }
