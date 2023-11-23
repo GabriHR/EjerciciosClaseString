b613d5c72353c855af999fc74f32ada6fe1d64d3
GabriHR <ghernrod@myuax.com>
2023-11-23 12:36:39 +0100

    Ejecicio3

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -3,19 +3,11 @@
 using namespace std;
 
 int main() {
-    string frase = "If you want to be a great programmer, you have to be strict";
-    string subcadena = "programmer";
-    string nueva_subcadena = "developer";
+    string cadena1 = "Buenas tardes, ";
+    string cadena2 = "Ruben!";
+    string cadena3 = cadena1 + cadena2;
 
-    size_t posicion = frase.find(subcadena);
-    if(posicion != string::npos) {
-        cout << "La subcadena se encontro en el indice: " << posicion << endl;
-        frase.replace(posicion, subcadena.length(), nueva_subcadena);
-    } else {
-        cout << "La subcadena no se encontro." << endl;
-    }
-
-    cout << "La cadena resultante es: " << frase << endl;
+    cout << "La cadena resultante es: " << cadena3 << endl;
 
     return 0;
 }
